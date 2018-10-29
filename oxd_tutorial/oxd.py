e0cbd61979cd6cacb561fecd704fa0712cc7cd85
Mustafa Baser <mbaser@mail.com>
2018-10-29 18:32:06 +0300

    Add files via upload

---
+++ oxd_tutorial/oxd.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+import requests
+import os
+import json
+import cgi
+
+oxd_id = '6179a64f-bfe3-44b3-85b5-c01f86336ef7'
+client_secret = 'd384ec9b-00a0-46cf-a92f-fea1225b0717'
+client_id = '@!5856.8A6C.09D4.C454!0001!655A.E96C!0008!D619.DF5D.B48E.DF10'
+print 'Content-type: text/html'
+print
+
+def post_data(end_point, data, access_token):
+    headers = {
+                'Content-type': 'application/json', 
+                'Authorization': "Bearer " + access_token
+        }
+
+    result = requests.post(
+                    'https://c3.gluu.org:8443/'+end_point, 
+                    data=json.dumps(data), 
+                    headers=headers, 
+                    verify=False
+                    )
+
+    return result.json()
+
+# We will use PATH_INFO which URI is requested
+path_info = os.environ.get('PATH_INFO','/')
+
+#If login is requested
+if path_info.startswith('/login'):
+
+    args = cgi.parse_qs(os.environ[ "QUERY_STRING" ])
+    
+    # Read access_token that we previously saved
+    access_token = open('/tmp/access_token.txt').read()
+
+    data = {
+        "oxd_id": oxd_id,
+        "code": args['code'][0],
+        "state": args['state'][0]
+    }
+
+    # Request access token to retreive user info. If you don't need user info
+    # It is enough to make sure the user logged in
+    result = post_data('get-tokens-by-code', data, access_token)
+
+    data = {
+        "oxd_id": oxd_id,
+        "access_token": result['access_token']
+    }
+
+    # Now we can retreive user information.
+    result = post_data('get-user-info', data, access_token)
+    
+    # Finally print user info
+    for cl in result['claims']:
+        print '<br><b>{0} :</b> {1}'.format(cl, result['claims'][cl][0])
+
+
+else:
+
+    #we need to get access_token from oxd-server
+    data = {
+      "op_host": "https://c2.gluu.org",
+      "scope": ["openid","oxd", "profile"],
+      "client_id": client_id,
+      "client_secret": client_secret,
+      "authentication_method": "",
+      "algorithm": "",
+      "key_id": ""
+    }
+
+    result = post_data('get-client-token', data, '')
+    access_token = result['access_token']
+
+    # Since cgi scripts runs per request, we need access_token in subsequent queries.
+    # Thus write a file. access_token should be secured and better to use session
+    # in web frameworks.
+    with open('/tmp/access_token.txt','w') as W:
+        W.write(access_token)
+
+    data = {
+      "oxd_id": oxd_id,
+      "scope": ["openid", "profile"],
+      "acr_values": ["basic"],
+    }
+
+    # User will be directed to Gluu Server to login, so wee need an url for login
+    result = post_data('get-authorization-url', data, access_token)
+
+    print '<a href="{0}">Click here to login</a>'.format(result['authorization_url'])
+
+
+
